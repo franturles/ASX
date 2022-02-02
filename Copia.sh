54574072e1541796bc562226aef899d7877912e2
franturles <kikofran35@hotmail.com>
2022-02-02 22:53:28 +0100

    Problemas con Logs

--- Copia.sh
+++ Copia.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-dt=`date +%y-%m-%d`
-tm="time +%h-%m"
-sudo mysqldump -u root -pabc123. --all-databases > CopiasDeSeguridad/CopiaBD$dt.sql
-sudo mkdir /home/skills/Copias/CopiaLogs/Logs$dt | cp /var/log/mysql/* /home/skills/Copias/CopiaLogs/Logs$dt
-sudo cp /etc/mysql/my.cnf /home/skills/Copias/FicheroConf$dt
+dt=`date +%d-%b-%y_%k:%M`
+sudo mysqldump -u root -pabc123. --all-databases > /home/Copias/CopiasDeSeguridad/$dt.sql
+sudo mkdir /home/Copias/CopiasLogs/$dt | sudo cp /var/log/mysql/* /home/Copias/CopiaLogs/$dt
+sudo cp /etc/mysql/my.cnf /home/Copias/CopiaFicheroConf/$dt
